c25f5be3fa50ef84cf23a62bc7eb7227890124f2
bergschneider <magiclake@gmx.de>
2023-08-30 03:35:21 +0200

    feat: console disclaimer

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,6 +20,18 @@ window.addEventListener('hashchange', () => {
     doDarkMode();
 });
 
+function warnConsole() {
+    console.warn("%cWarte", "font-size: 20px; font-weight: bold;");
+    console.warn("%cFalls dich jemand dazu aufgefordert hat, etwas zu kopieren und hier einzufügen, handelt es sich in 11 von 10 Fällen um einen Betrugsversuch.", "font-size: 16px;");
+    console.warn("%cEtwas hier einzufügen könnte Angreifern Zugriff auf deinen Account geben.", "font-size: 16px;");
+    console.warn("%cWenn du nicht ganz genau weißt, was du da tust, dann schließe das Fenster und bleib auf der sicheren Seite.", "font-size: 16px;");
+}
+
+setInterval(() => {
+    warnConsole();
+}, 5000);
+warnConsole()
+
 const app = new App({
     target: document.getElementById("app"),
 });
